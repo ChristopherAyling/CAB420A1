2dc4b0a8067bd6de07de2408971f1204f72b723b
Christopher <chris.ayling.360@gmail.com>
2018-04-08 14:12:27 +1000

    added report.m, did 2b, fixed some plots

---
+++ report.m
@@ -0,0 +1,65 @@
+%% 2018 - CAB420 - Machine Learning
+% Group Assignment 1
+% Alex Wilson and Christopher Ayling
+
+%% Theory 
+
+% (a)
+
+% (b)
+
+% (c)
+
+%% 1. Feature, Classes and Linear Regression
+
+% (a)
+
+% (b)
+
+% (c)
+
+% (d)
+
+% (e)
+
+% (f)
+
+%% 2. kNN Regression
+
+%% (a)
+% <include>@knnRegress/predict.m</include>
+
+%% (b)
+
+%% (c)
+
+%% 3. Hold-out and Cross-validation
+
+% (a)
+
+% (b)
+
+% (c)
+
+%% 4. Nearest Neighbor Classifiers
+
+% (a)
+
+% (b)
+
+% (c)
+
+% (d)
+
+%% 5. Perceptron and Logistic Regression
+
+% (a)
+
+% (b)
+
+% (c)
+
+% (d)
+
+% (e)
+
